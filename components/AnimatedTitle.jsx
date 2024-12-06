60c7da6708af20ab130c42c5e1c6dc64a226d268
Udita-bit <uditasingh2004@gmail.com>
2024-12-06 15:13:42 +0530

    Update AnimatedTitle.jsx

--- components/AnimatedTitle.jsx
+++ components/AnimatedTitle.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
 import gsap from 'gsap'
+import React ,{ useEffect } from 'react'
+
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import clsx from "clsx";
 gsap.registerPlugin(ScrollTrigger);
